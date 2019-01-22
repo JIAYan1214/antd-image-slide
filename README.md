71c8ef4876c598664e96161146f61695b64fe0cb
xuzeyuan <xuzeyuan@movikr.com>
2019-01-22 15:44:04 +0800

    add readme gif

--- README.md
+++ README.md
@@ -1,68 +1,57 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+#antd-image-slide
 
-## Available Scripts
+>基于 antd-modal+swiper4 的弹层图片轮播组件
 
-In the project directory, you can run:
+- *由于旋转功能修改了 swiper4 源码，采用cdn方式引入swiper:*
+```
+<link rel="stylesheet" href="http://public.fuyoukache.com/web/swiper.4.3.3.css" />
+<script src="http://public.fuyoukache.com/web/swiper-4.4.6.rotate.min.js"></script>
+```
+- *webpack配置:*
 
-### `npm start`
+`externals: {'Swiper':'Swiper'}`
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+yarn add antd-image-slide
 
-### `npm test`
+```
+import ImgSlide from 'antd-image-slide'
 
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            imgSlideVisible:false
+        }
+    }
+  render() {
+    const imgList = [
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548151173425&di=c10a3af9520255b16f1f488c01687826&imgtype=0&src=http%3A%2F%2Fpic2.ooopic.com%2F01%2F42%2F30%2F22b1OOOPIC9f.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548151173237&di=09a7f4adb033a5ba56ab7b8bcec16636&imgtype=0&src=http%3A%2F%2Fpic19.nipic.com%2F20120316%2F5721017_092601675127_2.jpg',
+        'https://3.swiper.com.cn/demo/img/largeNature3.jpg?1',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548151284909&di=cce6190fd279d5dbcd6761bc159b39ad&imgtype=0&src=http%3A%2F%2Fimg4q.duitang.com%2Fuploads%2Fitem%2F201303%2F15%2F20130315223944_EvRW3.thumb.700_0.jpeg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548151284907&di=71eed9b44c6daa9993ae35e08c2d5f2c&imgtype=0&src=http%3A%2F%2Fpic2.ooopic.com%2F10%2F59%2F48%2F63b1OOOPIC4f.jpg',
+        'https://3.swiper.com.cn/demo/img/largeNature3.jpg?2',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548151364786&di=fda1c37c728886e8c709dd180263907e&imgtype=0&src=http%3A%2F%2Fwww.pig66.com%2Fuploadfile%2F2017%2F1230%2F20171230032029905.png',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1548151364783&di=4635b743e1a26ebc4091319189e4c6ed&imgtype=0&src=http%3A%2F%2Fwww.ld12.com%2Fupimg358%2Fallimg%2Fc150925%2F14431F3Y0460-1Y3M.png',
+        'https://3.swiper.com.cn/demo/img/largeNature3.jpg'
+    ]
+    return (
+      <div className="App">
+        {this.state.imgSlideVisible?
+            <ImgSlide visible={this.state.imgSlideVisible}
+                      showIndex={4}
+                      imgList={imgList}
+                      onHide={()=>{
+                          this.setState({imgSlideVisible:false})
+                      }}/>
+            :null
+        }
+        <Button onClick={()=>{ this.setState({imgSlideVisible:true})} } >查看图片</Button>
+      </div>
+    );
+  }
+}
+```
 
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+![截图](https://raw.githubusercontent.com/zeyuang/antd-image-slide/master/readme.img/readme.gif)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,175 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/**
+ * Created by zeyuan on 2018/12/22
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Swiper from 'Swiper'
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Modal, Icon } from 'antd';
+import style from './index.less'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default class ImgSlide extends React.Component{
+    static defaultProps = {
+        visible:false,
+        showIndex:0,
+        imgList:[],
+        onHide:()=>{}
+    }
+    constructor(props){
+        console.log(props)
+        super(props)
+        this.state = {
+            visible: false,
+            degs: {}
+        }
+        this.swiper = null;
+    }
+
+    componentDidMount(){
+        setTimeout(()=>{this.swiperInit()},200)
+    }
+
+    componentWillUnmount(){
+        this.swiper.destroy(true,true)
+        this.swiperSmall.destroy(true,true)
+    }
+
+    hideModal = () => {
+        this.props.onHide()
+    }
+
+    swiperInit(){
+        if(this.swiper){
+            return
+        }
+        let degs = {}
+        this.props.imgList.forEach((item,index)=>{
+            degs[index] = {}
+            degs[index].deg = 0
+            degs[index].scale = 1
+        })
+        this.setState({
+            visible:true,
+            degs:degs
+        })
+        const {showIndex} = this.props
+        this.swiper = new Swiper('.swiper-container', {
+            observer:true,
+            touchRatio:0,
+            initialSlide:showIndex,
+            zoom: {
+                toggle: false,
+            },
+            spaceBetween: 10
+        });
+
+        this.swiperSmall = new Swiper('.swiper-container-small', {
+            initialSlide:showIndex,
+            spaceBetween: 12,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            loopedSlides:this.props.imgList.length,
+            touchRatio: 0,
+            slideToClickedSlide: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            }
+        });
+
+        this.swiper.controller.control = this.swiperSmall;
+        this.swiperSmall.controller.control = this.swiper;
+    }
+
+    swiperZoomIn = () =>{
+        let degs = this.state.degs
+        if(degs[this.swiper.activeIndex].scale === 3){
+            return;
+        }
+        this.swiper.zoom.in()
+        degs[this.swiper.activeIndex].scale = 3
+        this.setState({
+            degs:degs
+        })
+    }
+
+    swiperZoomOut = () =>{
+        let degs= this.state.degs
+        if(degs[this.swiper.activeIndex].scale === 1){
+            return;
+        }
+        this.swiper.zoom.out()
+        degs[this.swiper.activeIndex].scale = 1
+        this.setState({
+            degs:degs
+        })
+    }
+
+    swiperRotate = () =>{
+        let degs = this.state.degs
+        let deg = degs[this.swiper.activeIndex].deg
+        degs[this.swiper.activeIndex].deg = deg - 90
+        this.setState({
+            degs:degs
+        })
+        this.swiper.update(true)
+    }
+
+    render(){
+        const { imgList } = this.props;
+
+        return(
+            <Modal
+                style={{'top':'50px'}}
+                visible={this.props.visible}
+                footer={null}
+                onOk={this.hideModal}
+                onCancel={this.hideModal}
+                width={820}
+                className="swiper_modal"
+                destroyOnClose={true}
+            >
+                <div className="swiper_modal_body" >
+                    <div className="swiper-container" style={{'opacity':(this.state.visible?1:0)}}>
+                        <div className="swiper-wrapper">
+                            {imgList ? imgList.map((item,index) =>{
+                                const pic = this.state.degs[index] || {}
+                                return(
+                                    <div className="swiper-slide" key={item}>
+                                        <div className="swiper-zoom-container">
+                                            <img alt='' src={item} rotate={(pic.deg || 0)} style={{'transform': 'rotate(' + (pic.deg || 0) + 'deg) scale('+(pic.scale || 1)+')'}}/>
+                                        </div>
+                                    </div>
+                                )}
+                            ):null}
+                        </div>
+                    </div>
+                    <div className='swiper-container-small-container'>
+                        <div className="swiper-container-small">
+                            <div className="swiper-wrapper">
+                                {imgList ? imgList.map((item,index) =>{
+                                    const pic = this.state.degs[index] || {}
+                                    return(
+                                        <div className="swiper-slide" key={item+'_small'}>
+                                            <div className="swiper-zoom-container">
+                                                <img alt="" src={item} style={{'transform': 'rotate(' + (pic.deg || 0) + 'deg)'}}/>
+                                            </div>
+                                        </div>
+                                    )}
+                                ):null}
+                            </div>
+                        </div>
+                        <a className="swiper-button-prev"></a>
+                        <a className="swiper-button-next"></a>
+                    </div>
+
+                    <div className="control">
+                        <span className='a' onClick={this.swiperZoomIn}><Icon type="zoom-in" /></span>
+                        <span className='a' onClick={this.swiperZoomOut}><Icon type="zoom-out" /></span>
+                        <span className='a' onClick={this.swiperRotate}><Icon type="undo" /></span>
+                    </div>
+                </div>
+            </Modal>
+        )
+    }
+}
